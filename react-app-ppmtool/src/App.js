6f0987f619458ea80e0d9bbd7a47089017e3255d
svrohith9 <svrohith999@outlook.com>
2020-04-15 01:07:20 +0530

    node modules and homepage

--- react-app-ppmtool/src/App.js
+++ react-app-ppmtool/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/layout/Header";
 function App() {
