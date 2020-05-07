9b887df6f924204a7de84dd2e1d078106fad85d5
svrohith9 <svrohith999@outlook.com>
2020-05-07 22:03:46 +0530

    CRUD -UI updates

--- crud-ui/src/App.js
+++ crud-ui/src/App.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Hello World</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8000/users")
+      .then((response) =>
+        this.setState({ users: response.data, isLoading: true })
+      );
+  }
+
+  render() {
+    let { isLoading, users } = this.state;
+
+    if (!isLoading) {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <p>Loading...</p>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>
+                  Name :{user.name} | mobile: {user.mobile} | age {user.age}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
--- crudapp/src/main/java/com/crudapp/crudapp/controllers/UsersController.java
+++ crudapp/src/main/java/com/crudapp/crudapp/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.crudapp.crudapp.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.crudapp.crudapp.beans.Users;
 import com.crudapp.crudapp.repository.UsersRepository;
 
+
 @RestController
 public class UsersController {
 
 	@Autowired
 	private UsersRepository usersRepository;
-
+	
+	@CrossOrigin(origins = {"http://localhost:3000"})
 	@GetMapping("/users")
 	public List<Users> getAllUsers() {
 		return usersRepository.findAll();
