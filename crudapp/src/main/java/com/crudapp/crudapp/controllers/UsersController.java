8468bc8f43d02877c43d7c04ef11938d04598b9e
svrohith9 <svrohith999@outlook.com>
2020-05-09 20:39:48 +0530

    Update UsersController.java

--- crudapp/src/main/java/com/crudapp/crudapp/controllers/UsersController.java
+++ crudapp/src/main/java/com/crudapp/crudapp/controllers/UsersController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.crudapp.crudapp.beans.Users;
@@ -15,12 +16,13 @@ import com.crudapp.crudapp.repository.UsersRepository;
 
 
 @RestController
+@RequestMapping("/")
+@CrossOrigin("*")
 public class UsersController {
 
 	@Autowired
 	private UsersRepository usersRepository;
 	
-	@CrossOrigin(origins = {"http://localhost:3000"})
 	@GetMapping("/users")
 	public List<Users> getAllUsers() {
 		return usersRepository.findAll();
